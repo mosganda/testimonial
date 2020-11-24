b4b0115925ecc638729c3da68f83b4a406042361
Ojiko <mosesojiko@yahoo.com>
2020-11-24 13:43:28 +0100

    modified testimonial

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const customer_image = document.querySelector('#image');
-const customer_name = document.querySelector('#name');
-const customer_Text = document.querySelector('#quote');
+const customer_image = document.querySelector('#customer-img');
+const customer_name = document.querySelector('#customer-name');
+const customer_Text = document.querySelector('#customer-text');
 const buttons = document.querySelectorAll('.btn');
 const Testimonials = [
     {
-        testimonial: "Thank you so much for a job well done.",
+        testimonial: "Thank you so much for a job well done. Keep it up guys..",
         authur: "John",
         image: "./images/emoji1.jpg"
     },
     {
-        testimonial:"You guys are the best! Keep up the great work!",
+        testimonial:"You guys are the best! I love your work. Keep up the great work!",
         authur: "Mark",
         image:"./images/emoji2.jpg"
     },
@@ -33,7 +33,7 @@ const Testimonials = [
 let counter = 0;
 buttons.forEach(element=>{
 element.addEventListener('click', function(){
-    if(element.classList.contains('prev')){
+    if(element.classList.contains('btnPrev')){
         counter--;
         if(counter < 0){
             counter = Testimonials.length -1;
@@ -42,7 +42,7 @@ element.addEventListener('click', function(){
         customer_name.textContent = Testimonials[counter].authur;
         customer_Text.textContent = Testimonials[counter].testimonial;
     }
-    if(element.classList.contains('next')){
+    if(element.classList.contains('btnNext')){
         counter++;
         if(counter >Testimonials.length-1){
             counter = 0;
